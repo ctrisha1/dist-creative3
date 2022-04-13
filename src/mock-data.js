f6c306b1338e90dfc7e23ac8fb38bcbe7e599c3b
ctrisha1 <90875645+ctrisha1@users.noreply.github.com>
2022-04-13 00:12:10 -0600

    Add files via upload

---
+++ src/mock-data.js
@@ -0,0 +1,158 @@
+let mock = [{
+  id: 1,
+  name: "Floral Tank",
+  price: "$5.62",
+  category: "Tops",
+  image: "floralTank.jpg"
+},
+{
+  id: 2,
+  name: "Purple Pants",
+  price: "$50.00",
+  category: "Bottoms",
+  image: "purplePants.jpeg"
+},
+{
+  id: 3,
+  name: "Orange Top",
+  price: "$5.92",
+  category: "Tops",
+  image: 'yellowTop.jpeg'
+},
+{
+  id: 4,
+  name: "Floral Set",
+  price: "$40.58",
+  category: "Tops",
+  image: 'floralSet.jpeg'
+},
+{
+  id: 5,
+  name: "Green Dress",
+  price: "$40.00",
+  category: "Dresses",
+  image: "greenDress.jpg"
+
+},
+{
+  id: 6,
+  name: "Pink Skirt",
+  price: "$30.77",
+  category: "Bottoms",
+  image: 'pinkSkirt.jpg'
+},
+{
+  id: 7,
+  name: "Yellow Skirt",
+  price: "$20.00",
+  category: "Bottoms",
+  image: 'yellowSkirt.jpg'
+},
+{
+  id: 8,
+  name: "Blue Dress",
+  price: "$40.00",
+  category: "Dresses",
+  image: 'blueDress.jpg'
+},
+{
+  id: 9,
+  name: "Yellow Floral Skirt",
+  price: "$24.76",
+  category: "Bottoms",
+  image: 'yellowFloralSkirt.jpg'
+
+},
+{
+  id: 10,
+  name: "Jeans",
+  price: "$62.50",
+  category: "Bottoms",
+  image: 'jeans.jpeg'
+},
+{
+  id: 11,
+  name: "Tie Top",
+  price: "$6.70",
+  category: "Tops",
+  image: "orangeTop.jpeg"
+},
+{
+  id: 12,
+  name: "Floral Dress",
+  price: "$40.00",
+  category: "Dresses",
+  image: "floralDress.jpg"
+},
+{
+  id: 13,
+  name: "Brown Pants",
+  price: "$36.92",
+  category: "Bottoms",
+  image: "brownPants.jpg"
+},
+{
+  id: 14,
+  name: "Fluff Bag",
+  price: "$25.36",
+  category: "Accessories",
+  image: "fluffBag.jpg"
+},
+{
+  id: 15,
+  name: "Graphic Top",
+  price: "$4.18",
+  category: "Tops",
+  image: "graphicTee.jpeg"
+},
+{
+  id: 16,
+  name: "Bee Earrings",
+  price: "$9.18",
+  category: "Accessories",
+  image: "beeEarrings.jpg"
+},
+{
+  id: 17,
+  name: "Purple Sweatpants",
+  price: "$4.73",
+  category: "Bottoms",
+  image: "whiteShoes.jpg"
+},
+{
+  id: 18,
+  name: "Cloud Dress",
+  price: "$50.00",
+  category: "Dresses",
+  image: "cloudDress.jpg"
+},
+{
+  id: 19,
+  name: "Flowy Top",
+  price: "$36.81",
+  category: "Tops",
+  image: "flowyTop.jpeg"
+},
+{
+  id: 20,
+  name: "Blue Top",
+  price: "$8.62",
+  category: "Tops",
+  image: "blueTop.jpg"
+},
+{
+  id: 21,
+  name: "Brown Nikes",
+  price: "$120.00",
+  category: "Shoes",
+  image: "nikeShoes.jpg"
+},
+{
+  id: 22,
+  name: "Platform Black Heels",
+  price: "$210.00",
+  category: "Shoes",
+  image: "versaceHeels.jpg"
+}
+]
+export default mock;
---
+++ src/views/BrowseView.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div class="pure-menu pure-menu-horizontal">
+    <ul class="pure-menu-list">
+      <li class="pure-menu-item"><a @click="select('Tops')" href="#" class="pure-menu-link">Tops</a></li>
+      <li class="pure-menu-item"><a @click="select('Dresses')" href="#" class="pure-menu-link">Dresses</a></li>
+      <li class="pure-menu-item"><a @click="select('Accessories')" href="#" class="pure-menu-link">Accessories</a></li>
+      <li class="pure-menu-item"><a @click="select('Bottoms')" href="#" class="pure-menu-link">Bottoms</a></li>
+      <li class="pure-menu-item"><a @click="select('Shoes')" href="#" class="pure-menu-link">Shoes</a></li>
+    </ul>
+  </div>
+  <ProductList :products="products" />
+</div>
+</template>
+
+<script>
+import ProductList from "../components/ProductList.vue"
+export default {
+  name: 'BrowseView',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+      category: '',
+    }
+  },
+  computed: {
+    products() {
+      return this.$root.$data.products.filter(product => product.category === this.category);
+    }
+  },
+  methods: {
+    select(category) {
+      this.category = category;
+    }
+  }
+}
+</script>
